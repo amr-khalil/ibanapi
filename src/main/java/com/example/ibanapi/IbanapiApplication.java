efc75860fc3c80ddf43257b748a424a55c336dc1
Amr Khalil <amr.alshemaly93@gmail.com>
2024-12-03 19:57:15 +0100

    initial commit

---
+++ src/main/java/com/example/ibanapi/IbanapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbanapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ibanapi/IbanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbanapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
